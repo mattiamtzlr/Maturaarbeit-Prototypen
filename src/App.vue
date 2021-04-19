c60eea63d694e285f3bb23967cf1c3d74c32fa3c
MaGaMe19 <mattia.metzler@gmail.com>
2021-04-19 15:48:58 +0200

    initial commit

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,30 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <h1>Hello {{name}}</h1>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 
 export default {
   name: 'App',
-  components: {
-    HelloWorld
+  components: {},
+  setup() {
+    return {
+      name: "Mattia",
+    }
   }
 }
 </script>
 
 <style>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
+  font-family: monospace;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
+  color: #386088;
   margin-top: 60px;
+}
+h1 {
+  font-size: 3em;
 }
 </style>
--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="hello">
-    <h1>{{ msg }}</h1>
+    <h2>{{ msg }}</h2>
     <p>
       For a guide and recipes on how to configure / customize this project,<br>
       check out the
@@ -37,6 +37,9 @@ export default {
     msg: String
   }
 }
+
+let name = "Mattia";
+console.log(name);
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
@@ -53,6 +56,6 @@ li {
   margin: 0 10px;
 }
 a {
-  color: #42b983;
+  color: #0a9657;
 }
 </style>
